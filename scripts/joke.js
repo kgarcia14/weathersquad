48c148894c01d50db766a90e12e7c9481add81d7
Sean R MacEachern <sean.r.maceachern@gmail.com>
2021-01-19 17:08:36 -0500

    added second api js file

---
+++ scripts/joke.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const generateJoke = document.querySelector('#generateWeather')
+
+generateJoke.addEventListener('submit', event => {
+    event.preventDefault();
+    getJoke()
+})
+
+function getJoke() {
+    const url = 'https://official-joke-api.appspot.com/jokes/random';
+    get(url).then(function (response) {
+        updateJokeBody(response.setup, response.punchline);
+        // updateJokePunch(response)
+        console.log(response)
+    })
+}
+
+function updateJokeBody(setup, punchline) {
+    const div6 = document.querySelector('#generateSetup');
+    div6.innerHTML = `
+    <h3>${setup}</h3>
+    <p>${punchline}</p>`;
+}
